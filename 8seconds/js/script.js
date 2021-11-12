d1da868a322bb3293c7f2b7106bb9fbdd29f8108
Heonchankim <khc9274@naver.com>
2021-11-12 23:30:55 +0900

    Edited after the tenth feedback

--- 8seconds/js/script.js
+++ 8seconds/js/script.js
@@ -1,4 +1,7 @@
 window.addEventListener("load",function () {
+    setTimeout(function () {
+        scrollTo(0,0)
+    }, 10);
     //-----------------------------------------------------------------------------------------------------
     // 헤더
     const header = document.querySelector("#header");
@@ -9,22 +12,86 @@ window.addEventListener("load",function () {
     header.addEventListener("mouseenter", headerChange);
     header.addEventListener("mouseleave", headerRemove);
     
+    window.addEventListener("scroll", function(event) {
+        event.preventDefault();
+        if(window.scrollY > 0){
+            headerChange();
+        }
+        else{
+            headerRemove()
+        }
+    });
     
-
     // 헤더 mouseenter/mouseleave 함수
     function headerChange() {
-        console.log("asbdsf")
         header.style.backgroundColor = "#ac1f2d";
         header.style.transitionDuration = duration+"ms";
         logo.setAttribute("src", "./images/mainLogosub.png");
 
     }
     function headerRemove() {
-        header.style.backgroundColor = "";
-        header.style.transitionDuration = duration+"ms";
-        logo.setAttribute("src", "./images/mainLogo.png");
-
+        if(window.scrollY > 0){
+            headerChange();
+        }
+        else{
+            header.style.backgroundColor = "";
+            header.style.transitionDuration = duration+"ms";
+            logo.setAttribute("src", "./images/mainLogo.png");
+        }
     }
+    //------------------------------------------------------------------------------------------------------
+    // 메인 슬라이드
+    const mainSlide = document.querySelector(".mainSlide");
+    const slideLeft = document.querySelector(".slideLeft");
+    const slideRight = document.querySelector(".slideRight");
+    const offsetTime = 5000;
+
+    // 슬라이드 좌우버튼 클릭 이벤트
+    slideRight.addEventListener("click", function () {
+        if(mainSlide.style.left == "-200%") return false;
+        window.clearInterval(timerId);
+        mainSlideRight();
+        timerId = window.setInterval(mainSlideRight, offsetTime);
+    });
+    slideLeft.addEventListener("click", function () {
+        if(mainSlide.style.transitionDuration == duration+"ms") return false;
+        window.clearInterval(timerId);
+        mainSlideLeft();
+        timerId = window.setInterval(mainSlideRight, offsetTime);
+    });
+
+    // 슬라이드 후버시 슬라이드 멈춤 이벤트
+    mainSlide.addEventListener("mouseenter", function () {
+        window.clearInterval(timerId);
+    });
+    mainSlide.addEventListener("mouseleave", function () {
+        timerId = window.setInterval(mainSlideRight, offsetTime);
+    });
+
+    let timerId = window.setInterval(mainSlideRight, offsetTime);
+
+    // 오른쪽이동 슬라이드 함수
+    function mainSlideRight() {
+        mainSlide.style.left ="-200%";
+        mainSlide.style.transitionDuration = duration + "ms";
+        
+        window.setTimeout(function () {
+            mainSlide.removeAttribute("style")
+            mainSlide.appendChild(mainSlide.firstElementChild);
+        }, duration);
+    }
+    
+    // 왼쪽이동 슬라이드 함수
+    function mainSlideLeft() {
+        mainSlide.style.left ="0";
+        mainSlide.style.transitionDuration = duration + "ms";
+        
+        window.setTimeout(function () {
+            mainSlide.removeAttribute("style")
+            mainSlide.insertBefore(mainSlide.lastElementChild, mainSlide.firstElementChild)
+        }, duration);
+    }
+    
     //------------------------------------------------------------------------------------------------------
     //------------------------------------------------------------------------------------------------------
     //------------------------------------------------------------------------------------------------------
